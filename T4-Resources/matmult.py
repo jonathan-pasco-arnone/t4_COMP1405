e03f832f98aaebb99e658687c0035a1f734e417c
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-09-29 17:04:26 +0000

    scalar complete

--- T4-Resources/matmult.py
+++ T4-Resources/matmult.py
@@ -1,5 +1,13 @@
 def mult_scalar(matrix, scale):
-	return [[]]
+	""" Multiplies the matrix by a scalar """
+	new_matrix = []
+	row = 0
+	for row_values in matrix:
+		new_matrix.append([])
+		for value in row_values:
+			(new_matrix[row]).append(scale * value)
+		row += 1
+	return new_matrix
 
 def mult_matrix(a, b):
 	return [[]]
